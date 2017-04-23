a1634413dfa11d67646782e61160c7a9b1c6d89d
Christian Bolstad <christian@bolstad.se>
2017-04-23 20:49:02 +0200

    example in phpdoc

--- src/PlaceholderCalc.php
+++ src/PlaceholderCalc.php
@@ -8,6 +8,19 @@
 
 namespace Bolstad\TextCalculate {
 
+    /**
+     * Class PlaceholderCalc
+     * @package Bolstad\TextCalculate
+     *
+     * Perform calculation on strings and replace placeholders according as configured in an array.
+     *
+     * Example:
+     *
+     *  Given that ITEMS_SOLD = 10 & TAX_RATE = 0.25 AND PROCESS_FEE = 3, calculate() can
+     *  solve '((ITEMS_SOLD * 15) + PROCESS_FEE) * 0.25'
+     *
+     * Please see `tests/PlaceholderCalcTest.php` for example code
+     */
 
     class PlaceholderCalc
     {

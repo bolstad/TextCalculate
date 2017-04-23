215932ac97f0fe17cef57d15d9bfb05d5663415c
Christian Bolstad <christian@bolstad.se>
2017-04-23 20:45:06 +0200

    tidy up

--- src/PlaceholderCalc.php
+++ src/PlaceholderCalc.php
@@ -6,41 +6,32 @@
  * Time: 19:58
  */
 
-namespace Bolstad\TextCalculate;
+namespace Bolstad\TextCalculate {
 
 
-class PlaceholderCalc
-{
-
-    /** Run match calculations on $input and do a string replace on each key in $placeholders
-     *  with it's value.
-     *
-     * $input example = '((12 * ITEMS_SOLD) + 0.25) * TAX_RATE'
-     * $placeholders = ['ITEMS_SOLD'=>3, 'TAX_RATE' = '0.25']
-     *
-     * @param $input
-     * @param array $placeholders
-     */
-    public function calculate($input, $placeholders = array())
+    class PlaceholderCalc
     {
 
+        /** Run match calculations on $input and do a string replace on each key in $placeholders
+         *  with it's value.
+         *
+         * $input example = '((12 * ITEMS_SOLD) + 0.25) * TAX_RATE'
+         * $placeholders = ['ITEMS_SOLD'=>3, 'TAX_RATE' = '0.25']
+         *
+         * @param $input
+         * @param array $placeholders
+         */
+        public function calculate($input, $placeholders = array())
+        {
+            foreach ($placeholders as $placeholder => $value) {
+                $input = str_replace($placeholder, $value, $input);
+            }
 
+            $calc = new FieldCalc();
+            $result = $calc->calculate($input);
 
-        var_dump($placeholders);
-
-        echo "pre input is: $input\n";
-
-        foreach ($placeholders as $placeholder => $value) {
-            $input = str_replace($placeholder,$value,$input);
+            return $result;
         }
-        echo "post input is: $input\n";
 
-        $calc = new FieldCalc();
-
-        $result = $calc->calculate($input);
-
-        echo "result: $result\n";
-        return $result;
     }
-
 }
